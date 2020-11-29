1db3d7f6f3cfec7307036817d0b758a7abcd0fbf
koftov <ahronlu@gmail.com>
2020-11-29 19:55:00 +0200

    loader

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import useFetchJobs from './useFetchJobs'
-import { Container } from 'react-bootstrap'
-import Job from './Job'
-import JobsPagination from './JobsPagination';
-import SearchForm from './SearchForm';
+import React, { useState } from "react";
+import useFetchJobs from "./useFetchJobs";
+import { Container, Spinner } from "react-bootstrap";
+import Job from "./Job";
+import JobsPagination from "./JobsPagination";
+import SearchForm from "./SearchForm";
 
 function App() {
-  const [params, setParams] = useState({})
-  const [page, setPage] = useState(1)
-  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
   function handleParamChange(e) {
-    const param = e.target.name
-    const value = e.target.value
-    setPage(1)
-    setParams(prevParams => {
-      return { ...prevParams, [param]: value }
-    })
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
   }
 
   return (
@@ -24,14 +24,14 @@ function App() {
       <h1 className="mb-4">GitHub Jobs</h1>
       <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
-      {loading && <h1>Loading...</h1>}
+      {loading && <Spinner animation="border" />}
       {error && <h1>Error. Try Refreshing.</h1>}
-      {jobs.map(job => {
-        return <Job key={job.id} job={job} />
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
       })}
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
-  )
+  );
 }
 
 export default App;
